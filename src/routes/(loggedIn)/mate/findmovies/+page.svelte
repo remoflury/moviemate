9e7d234cb5b4b84cbcba79e8effb142ccce1e1e1
remoflury <remoflury97+web@gmail.com>
2023-09-25 13:19:11 +0200

    push for team

--- src/routes/(loggedIn)/mate/findmovies/+page.svelte
+++ src/routes/(loggedIn)/mate/findmovies/+page.svelte
@@ -4,22 +4,7 @@
 	// $: console.log(data.matches);
 	// $: console.log(data.recommendations);
 
-	// console.log(data.data)
-	// 1008042  Talk to Me
-	// 385687 Fast X
-	// 346698 Barbie
-	// 615656 Meg 2: The Trench
-	// 968051 The Nun II
-
-	// const movies1 = [
-	// '1008042'
-	// 	'385687',
-	// 	'346698',
-	// 	'615656',
-	// 	'968051'
-	// ] // Spiderpig
-
-	// const movies2 = ['1008042', '615656', '968051'] // Spiderman
+	console.log(data);
 </script>
 
 <h2>Matches</h2>

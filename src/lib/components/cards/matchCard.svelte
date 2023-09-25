687524440e066d25173bfee44e40721aebb7d803
Chrisinii <christina.benz@stud.fhgr.ch>
2023-09-25 15:37:11 +0200

    styling match tag v1

--- src/lib/components/cards/matchCard.svelte
+++ src/lib/components/cards/matchCard.svelte
@@ -4,9 +4,13 @@
 	export let content: TMDBMovieByIdrops;
 </script>
 
-<figure class="rounded-card overflow-hidden relative">
+<figure class="rounded-card overflow-hidden relativ">
 	<img
 		src="https://image.tmdb.org/t/p/original/{content.poster_path}"
 		alt="Movieposter of {content.title}"
 	/>
+
+    <span class="absolute insert-x-0 bottom-0 left-1/2"><p>match</p></span>
+
 </figure>
+

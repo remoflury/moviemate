9d68390f141bec11423759cf8f8fc16ee6a9edd1
remoflury <remoflury97+web@gmail.com>
2023-09-26 18:30:42 +0200

    moved logout button

--- src/routes/(loggedIn)/profile/+page.svelte
+++ src/routes/(loggedIn)/profile/+page.svelte
@@ -1,3 +1,14 @@
+<script lang="ts">
+	import { enhance } from '$app/forms';
+	import { page } from '$app/stores';
+	import PrimaryButton from '$lib/components/primaryButton.svelte';
+</script>
+
 <section class="container">
 	<h1>Profile</h1>
+	{#if $page.data.session}
+		<form method="POST" action="/logout?/logout" use:enhance class="mt-8">
+			<PrimaryButton text="Logout" />
+		</form>
+	{/if}
 </section>
--- src/routes/(loggedIn)/swipe/+page.svelte
+++ src/routes/(loggedIn)/swipe/+page.svelte
@@ -10,10 +10,4 @@
 
 <section class="container">
 	<h1>Swipe</h1>
-
-	{#if $page.data.session}
-		<form method="POST" action="/logout?/logout" use:enhance class="mt-8">
-			<PrimaryButton text="Logout" />
-		</form>
-	{/if}
 </section>

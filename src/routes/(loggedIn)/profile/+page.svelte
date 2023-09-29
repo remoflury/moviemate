773b53b2b898459b3e2c52d66cb68ffa51a917f0
tinafrauenknecht <tinafrauenknecht@users.noreply.github.com>
2023-09-29 19:45:14 +0200

    Profile Avatar hinzugefügt

--- src/routes/(loggedIn)/profile/+page.svelte
+++ src/routes/(loggedIn)/profile/+page.svelte
@@ -2,13 +2,22 @@
 	import { enhance } from '$app/forms';
 	import { page } from '$app/stores';
 	import PrimaryButton from '$lib/components/primaryButton.svelte';
+	import Avatar from '$lib/components/mates/avatar.svelte';
+
 </script>
 
 <section class="container">
-	<h1>Profile</h1>
+	<div class="flex flex-col items-center px-32 pb-10">
+			<Avatar />
+	<p class="info mt-4">Username??</p>
+</div>
+
+	<h1>Watchlist</h1>
+
 	{#if $page.data.session}
 		<form method="POST" action="/logout?/logout" use:enhance class="mt-8">
 			<PrimaryButton text="Logout" />
 		</form>
 	{/if}
+
 </section>

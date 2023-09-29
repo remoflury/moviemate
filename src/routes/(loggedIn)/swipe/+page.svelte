0b83c3a0b61d4d3c463875759e15f545ba61b2ea
remoflury <remoflury97+web@gmail.com>
2023-09-29 09:09:43 +0200

    reduced unneccessary code

--- src/routes/(loggedIn)/swipe/+page.svelte
+++ src/routes/(loggedIn)/swipe/+page.svelte
@@ -8,14 +8,9 @@
 	import { onMount } from 'svelte';
 
 	export let data;
-	const movies = data.movies;
-	// console.log(data.movies);
+	let movies = data.movies;
 
-	let moreMovies: TMDBMovieByRecommendationProps[] = [];
-	let initialMoviesDone: boolean = false;
 	let loading: boolean = false;
-
-	// $: console.log(moreMovies);
 
 	let countIndex = 0;
 
@@ -38,21 +33,20 @@
 
 	const onSwipe = async () => {
 		countIndex = countIndex + 1;
-		console.log('countIndex ', countIndex);
-		console.log('movies.length ', movies.length);
 
 		// if no more movies are available, load more movies
-		//TODO: what if the loaded more movies are swept? how are more loaded?
 		if (countIndex + 1 === movies.length) {
 			loading = true;
 			try {
-				moreMovies = await fetchMoreMovies();
+				movies = await fetchMoreMovies();
+				console.log(movies);
 			} catch (error) {
 				throw new Error('An error occured while loading more movies');
 			}
+			movies = [...movies];
+
 			loading = false;
 			countIndex = 0;
-			initialMoviesDone = true;
 		}
 	};
 </script>
@@ -60,25 +54,13 @@
 <section class="container">
 	<h1 hidden>Swipe</h1>
 	<div class="relative">
-		<!-- for initially loaded movies -->
 		{#each movies as movie, index (index)}
-			{#if index === countIndex && !initialMoviesDone}
+			{#if index === countIndex}
 				<div in:fade={{ duration: 350 }}>
 					<SwipeCard {movie} {index} on:swipeRight={onSwipe} on:swipeLeft={onSwipe} />
 				</div>
 			{/if}
 		{/each}
-		<!-- for more loaded movies -->
-		{#if moreMovies.length && initialMoviesDone}
-			{#each moreMovies as movie, index (index)}
-				{#if index === countIndex}
-					<div in:fade={{ duration: 350 }}>
-						<SwipeCard {movie} {index} on:swipeRight={onSwipe} on:swipeLeft={onSwipe} />
-					</div>
-					<!-- <p>{movie.title}</p> -->
-				{/if}
-			{/each}
-		{/if}
 		{#if loading}
 			<LoadingSpinner />
 		{/if}

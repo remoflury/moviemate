d9294a08c91d2994ea2ab8b6aa7824d20ef4cddd
tinafrauenknecht <tinafrauenknecht@users.noreply.github.com>
2023-10-02 11:24:05 +0200

    Watchlist Cards

---
+++ src/lib/components/cards/watchlistCard.svelte
@@ -0,0 +1,38 @@
+<!-- <script lang="ts">
+	import type { TMDBMovieByIdrops, TMDBMovieByRecommendationProps } from '$lib/types/contentTypes';
+    import {movieStore} from '$lib/stores/movieStore';
+	export let showDeleteButton: boolean = true;
+	export let content: TMDBMovieByIdrops | TMDBMovieByRecommendationProps;
+
+    const removeMovieFromWatchlist = (id: string) => {
+		// find index of movie in store
+		const index = $movieStore.movies.findIndex((movie) => movie.id === id);
+
+		// if movie is in store, remove it
+		if (index !== -1) {
+			$movieStore.movies.splice(index, 1);
+		}
+	};
+
+</script>
+
+<a href="/mates/findmovies/{content.id}">
+	<figure class="rounded-3xl overflow-hidden relative">
+		<img
+			src="https://image.tmdb.org/t/p/w500/{content.poster_path}"
+			alt="Movieposter of {content.title}"
+			loading="lazy"
+		/>
+
+        {#if showDeleteButton}
+			<button
+				aria-label="remove movie"
+				class="absolute bottom-0 right-0 border border-gray-dark rounded-max p-[6px] aspect-square grid place-content-center bg-white translate-x-1/4 translate-y-1/4"
+				on:click={() => removeMovieFromWatchlist(content.id)}
+			>
+				<p class="text-gray-dark leading-none -translate-y-[1px]">-</p>
+			</button>
+		{/if}
+	</figure>
+    
+</a> -->
--- src/routes/(loggedIn)/profile/+page.svelte
+++ src/routes/(loggedIn)/profile/+page.svelte
@@ -3,21 +3,29 @@
 	import { page } from '$app/stores';
 	import PrimaryButton from '$lib/components/primaryButton.svelte';
 	import Avatar from '$lib/components/mates/avatar.svelte';
+	import WatchlistCard from '$lib/components/cards/watchlistCard.svelte';
+	export let data;
 
+	console.log(data);
 </script>
 
 <section class="container">
 	<div class="flex flex-col items-center px-32 pb-10">
-			<Avatar />
-	<p class="info mt-4">Username??</p>
-</div>
+		<Avatar />
+		<p class="info mt-4">Username??</p>
+	</div>
 
 	<h1>Watchlist</h1>
 
-	{#if $page.data.session}
+	<!-- <div class="grid grid-cols-3 gap-x-6 gap-y-10">
+		{#each data.movies as movie, index (index)}
+			<WatchlistCard content={movie} />
+		{/each}
+	</div> -->
+
+
 		<form method="POST" action="/logout?/logout" use:enhance class="mt-8">
 			<PrimaryButton text="Logout" />
 		</form>
-	{/if}
 
 </section>

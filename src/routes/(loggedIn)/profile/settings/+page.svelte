37bdf273648561a6c19bab655b377c8d92906538
remoflury <remoflury97+web@gmail.com>
2023-10-09 13:20:33 +0200

    restructure settings page

--- src/routes/(loggedIn)/profile/settings/+page.svelte
+++ src/routes/(loggedIn)/profile/settings/+page.svelte
@@ -6,15 +6,12 @@
 	import PrimaryButton from '$lib/components/primaryButton.svelte';
 	import Avatar from '$lib/components/mates/avatar.svelte';
 
-
 	$showSettings = false;
 	$showGoBack = true;
-
-
-
 </script>
 
 <section class="container relative">
+	<h1>Settings</h1>
 	<div class="flex flex-col items-center px-28 pb-5">
 		<Avatar />
 		<p class="info mt-4">{$page.data.user.username}</p>
@@ -25,19 +22,26 @@
 		</form>
 	</div>
 
-    <div class="pb-10">
-        <h1>Reference</h1>
-        <p>We used the Movie Database API https://developers.themoviedb.org/3</p>
-    </div>
+	<div class="pb-10">
+		<h2>Reference</h2>
 
-    <div class="pb-10">
-        <h1>Privacy</h1>
-        <p>Text Text Text</p>
-    </div>
+		<p>
+			We used <a
+				class="underline underline-offset-2"
+				href="https://developers.themoviedb.org/3"
+				target="blank"
+				>The Movie Database API
+			</a>
+		</p>
+	</div>
 
-    <div class="pb-10">
-        <h1>Legal Notice</h1>
-        <p>Text Text Text</p>
-    </div>
-	
+	<div class="pb-10">
+		<h2>Privacy</h2>
+		<p>Text Text Text</p>
+	</div>
+
+	<div class="pb-10">
+		<h2>Legal Notice</h2>
+		<p>Text Text Text</p>
+	</div>
 </section>

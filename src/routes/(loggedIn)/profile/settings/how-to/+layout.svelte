e1a9ab5c696c061f2ac2a7da5083a778cf4a5f33
tinafrauenknecht <tinafrauenknecht@users.noreply.github.com>
2023-10-16 11:02:39 +0200

    How-to

---
+++ src/routes/(loggedIn)/profile/settings/how-to/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { activeMenu, previousPath } from '$lib/stores/menu';
+    import BottomNavigation from '$lib/components/navigation/bottomNavigation.svelte';
+	import LogoHeader from '$lib/components/logoHeader.svelte';
+	$activeMenu = 'profile';
+	$previousPath.params = '';
+</script>
+
+<slot />
+<BottomNavigation />

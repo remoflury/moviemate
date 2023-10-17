7ee206baf5cbc155245f99015a23adc7c0285519
remoflury <remoflury97+web@gmail.com>
2023-10-17 10:41:52 +0200

    add page query param to search api

--- src/routes/api/searchmovie/+server.ts
+++ src/routes/api/searchmovie/+server.ts
@@ -4,12 +4,13 @@ import type { RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({fetch, url}) => {
   const searchQuery = url.searchParams.get('query')
+  const page = url.searchParams.get('page') || '1'
 
   // error handling
   if (!searchQuery) return new Response(JSON.stringify({ error: 'Please search for a movie.' }), { status: 404 })
 
   try {
-    const response = await fetch(`${TMDB_BASE_URL}/search/movie?query=${searchQuery}&language=en-US`, {
+    const response = await fetch(`${TMDB_BASE_URL}/search/movie?query=${searchQuery}&language=en-US&page=${page}`, {
       method: 'GET',
       headers: {
         accept: 'application/json',

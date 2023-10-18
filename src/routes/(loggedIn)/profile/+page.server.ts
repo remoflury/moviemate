7ae414ee3cb043c7d5bbcf0136885d8a8d6a3d8f
remoflury <remoflury97+web@gmail.com>
2023-10-18 10:20:26 +0200

    fetch watchlistIds

--- src/routes/(loggedIn)/profile/+page.server.ts
+++ src/routes/(loggedIn)/profile/+page.server.ts
@@ -1,9 +1,25 @@
 import { error as pageError } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { getAllMovieIds } from '$lib/utils/moviesUtils';
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const session = await locals.getSession();
-
 	// if not authorized
 	if (!session) throw pageError(401, 'Unauthorized. Please login.');
+
+	const supabaseClient = locals.supabase
+	const userId = session.user.id;
+
+	let watchlistIds: string[]
+	try {
+		const data = await getAllMovieIds(supabaseClient, [userId])
+		watchlistIds = data[0].movies_watchlist
+	} catch (err) {
+		console.error(err)
+		throw pageError(500, 'Ups, something went wrong.')
+	}
+
+	return {
+		watchlistIds
+	}
 };

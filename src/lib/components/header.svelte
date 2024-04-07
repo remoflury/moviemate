ce1dc93339bcee513e97c6968569bcf4d7332369
remoflury <remoflury97+web@gmail.com>
2024-04-07 21:16:28 +0200

    header

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+	import Logo from '$lib/assets/logo.svelte'
+</script>
+
+<header class="container grid grid-cols-4 gap-x-4 py-1 border-b border-gray-dark">
+	<figure class="col-span-2 col-start-2 max-w-1/2 mx-auto">
+		<Logo />
+	</figure>
+</header>
---
+++ src/routes/(app)/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Header from '$lib/components/header.svelte'
+</script>
+
+<Header />
+<slot />

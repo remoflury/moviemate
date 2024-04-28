fc470b75adba23f37fcf07073de2540635d22691
remoflury <remoflury97+web@gmail.com>
2024-04-28 13:45:41 +0200

    bottom nav

---
+++ src/lib/components/nav.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { page } from '$app/stores'
+	import MateIcon from '$lib/assets/mateIcon.svelte'
+	import SwipeIcon from '$lib/assets/swipeIcon.svelte'
+	import ProfileIcon from '$lib/assets/profileIcon.svelte'
+
+	$: firstUriSegment = $page.url.pathname.split('/')[1]
+</script>
+
+<nav class="fixed bottom-0 left-0 right-0">
+	<div class="container flex justify-evenly items-center gap-x-4 py-6 bg-blue">
+		<a class="max-w-[2rem]" href="/profile">
+			<ProfileIcon isActive={firstUriSegment === 'profile'} />
+		</a>
+		<a class="max-w-[2rem]" href="/swipe">
+			<SwipeIcon isActive={firstUriSegment === 'swipe'} />
+		</a>
+		<a class="max-w-[2.5rem]" href="/mate">
+			<MateIcon isActive={firstUriSegment === 'mate'} />
+		</a>
+	</div>
+</nav>
---
+++ src/routes/(app)/(loggedIn)/+layout.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+	import Nav from '$lib/components/nav.svelte'
+</script>
+
+<slot />
+<Nav />
